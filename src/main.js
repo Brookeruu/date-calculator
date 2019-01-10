4402698be511f1fc0f7b0bb0eb6ca1741c724592
Brooke Perry and Robert Bruce <robert@>
2019-01-10 15:31:35 -0800

    add UI

--- src/main.js
+++ src/main.js
@@ -5,4 +5,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
+
+  $("#dateInput").submit(function(event) {
+    event.preventDefault();
+
+    let month = parseInt($("#inputMonth").val());
+    let day = parseInt($("#inputDay").val());
+    let year = parseInt($("#inputYear").val());
+
+    let datechecker = new DateChecker(year, month, day);
+
+    if (datechecker.isInputValid()) {
+      $("#outputDay").text(datechecker.getWeekday());
+
+    } else  {
+      $("#outputDay").text("Please enter a vaid date!");
+    }
+
+
+  });
 });
